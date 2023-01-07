44fbd4521d5923d5d5d62adb83a6bde8c550dc97
Shadi Hamdan <shamdan17@ku.edu.tr>
2023-01-07 20:14:53 +0300

    Add PE dataset methods

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # concept-drift-robustness
+
+
+## Windows PE datasets:
+
+First download the data from the following links:
+
+### BODMAS
+```
+https://drive.google.com/drive/folders/1Uf-LebLWyi9eCv97iBal7kL1NgiGEsv_
+```
+
+### EMBER
+A version of EMBER preprocessed by BODMAS authors can be found here, only ember related files are needed:
+
+```
+https://drive.google.com/drive/folders/12DMPeh8DA2ukPATnHX4K__shWFJIiBN5
+```
+
+
---
+++ src/dataset.py
@@ -0,0 +1,140 @@
+import numpy as np
+import pandas as pd
+import copy
+
+PE_DATASETS = {
+    "bodmas": ("data/bodmas.npz", "data/bodmas_metadata.csv"),
+    "ember": ("data/ember.npz", "data/ember_metadata.csv"),
+}
+
+
+# Generic Malware Dataset class
+class MalwareDataset:
+    # Constructor
+    def __init__(self):
+        raise NotImplementedError
+
+    # Get feature width
+    def get_feature_width(self):
+        raise NotImplementedError
+
+    # Get the number of samples in the dataset
+    def __len__(self):
+        raise NotImplementedError
+
+    # Get a sample from the dataset given an index
+    def __getitem__(self, index):
+        raise NotImplementedError
+
+    # Filter dataset by date
+    def filter_by_date(self, start_date, end_date):
+        raise NotImplementedError
+
+    def filter_by_lambda(self, filter_lambda, include_label=False, include_index=False, include_date=False):
+        raise NotImplementedError
+
+class PEMalwareDataset(MalwareDataset):
+    """
+    Class for loading and filtering PE malware datasets
+    
+    Attributes:
+        path (str): Path to the dataset
+        metadata_path (str): Path to the metadata file
+
+        features (np.ndarray): Array of features
+        labels (np.ndarray): Array of labels
+        dates (np.ndarray): Array of dates
+
+    Usage:
+        # By path
+        dataset = PEMalwareDataset("data/bodmas.npz", "data/bodmas_metadata.csv")
+        # By name
+        dataset = PEMalwareDataset.from_name("bodmas")
+        # Filter by date
+        dataset = dataset.filter_by_date(201701, 201712)
+    """
+
+    # Constructor
+    def __init__(self, path, metadata_path):
+        self.path = path
+        self.metadata_path = metadata_path
+
+        # Load the dataset
+        dataset = np.load(path)
+
+        self.features = dataset['X']
+        self.labels = dataset['y']
+
+        # Load the metadata
+        metadata = pd.read_csv(metadata_path)
+
+        # Extract and concatenate the year and month
+        dates = [int(metadata.iloc[i][1][:7].replace("-", "")) for i in range(len(metadata))]
+
+        self.dates = np.array(dates)
+
+    # Get feature width
+    def get_feature_width(self):
+        return self.features.shape[1]
+
+    # Get the number of samples in the dataset
+    def __len__(self):
+        return len(self.features)
+
+    # Get a sample from the dataset given an index
+    def __getitem__(self, index):
+        return self.features[index], self.labels[index]
+
+    # Filter dataset by date
+    def filter_by_date(self, start_date, end_date):
+        # Filter by date
+        mask = np.logical_and(self.dates >= start_date, self.dates <= end_date)
+
+        # Perform shallow copy
+        selfcopy = copy.copy(self)
+
+        selfcopy.features = self.features[mask]
+        selfcopy.labels = self.labels[mask]
+        selfcopy.dates = self.dates[mask]
+
+        # Return the filtered dataset
+        return selfcopy
+
+    def filter_by_lambda(self, filter_lambda, include_label=False, include_index=False, include_date=False):
+        to_iterate = [self.features]
+
+        if include_label:
+            to_iterate.append(self.labels)
+
+        if include_index:
+            to_iterate.append(np.arange(len(self)))
+
+        if include_date:
+            to_iterate.append(self.dates)
+
+        mask = [filter_lambda(*args) for args in zip(*to_iterate)]
+
+        mask = np.array(mask)
+
+        # Perform shallow copy
+        selfcopy = copy.copy(self)
+
+        selfcopy.features = self.features[mask]
+        selfcopy.labels = self.labels[mask]
+        selfcopy.dates = self.dates[mask]
+
+        # Return the filtered dataset
+        return selfcopy
+
+    # Static method to instantiate by dataset name
+    @staticmethod
+    def from_name(name):
+        if name not in PE_DATASETS:
+            raise ValueError("Dataset not found")
+
+        return PEMalwareDataset(*PE_DATASETS[name])
+
+    def __repr__(self):
+        return "Dataset: {}\nInstances:{}".format(self.path, len(self))
+
+

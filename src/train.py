e6298f44e06fa72f028fea16b01184da7548e62c
Mısra Yavuz <misrayvz@gmail.com>
2023-01-10 01:29:27 +0300

    add more models and evaluation on train set

--- src/train.py
+++ src/train.py
@@ -12,7 +12,9 @@ from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 
 from dataset import PEMalwareDataset
@@ -21,10 +23,10 @@ from dataset import PEMalwareDataset
 def get_args_parser():
     parser = argparse.ArgumentParser('Training and evaluation script', add_help=False)
     parser.add_argument('--model_type', default='', type=str)
-    parser.add_argument('--train_start_date', default=201801, type=int, help='year in 4 digits followed by month in 2 digits')
+    parser.add_argument('--train_start_date', default=201908, type=int, help='year in 4 digits followed by month in 2 digits')
     parser.add_argument('--train_end_date', default=201912, type=int, help='year in 4 digits followed by month in 2 digits')
     parser.add_argument('--test_start_date', default=202001, type=int, help='year in 4 digits followed by month in 2 digits')
-    parser.add_argument('--test_end_date', default=202002, type=int, help='year in 4 digits followed by month in 2 digits')
+    parser.add_argument('--test_end_date', default=202004, type=int, help='year in 4 digits followed by month in 2 digits')
     parser.add_argument('--dataset', default='', type=str)
     parser.add_argument('--data_root', default='data/', type=str)
     parser.add_argument('--output_dir', default='exps/', type=str)
@@ -42,7 +44,8 @@ def main(args):
     counts = Counter(train_set.labels) 
     assert len(counts) > 1, 'Training data only has 1 class, consider expanding the time period'
     
-    X_train, X_val, y_train, y_val = train_test_split(train_set.features, train_set.labels, test_size=0.20, random_state=0) 
+    X_train, X_val, y_train, y_val = \
+        train_test_split(train_set.features, train_set.labels, test_size=0.20, random_state=0) 
     
     print('Dataset is loaded.')
     print('Train set size: {0}, Val. set size: {1}, Test set size: {2}'.format(X_train.shape[0], X_val.shape[0], test_set.labels.shape[0]))
@@ -52,8 +55,14 @@ def main(args):
         model = DecisionTreeClassifier(max_depth=5, random_state=0)
     elif args.model_type == 'XGBoost':
         model = XGBClassifier(max_depth=5, min_child_weight=1, gamma=0, learning_rate=0.1 ,n_estimators=100, random_state=0)
+    elif args.model_type == 'LR':
+        model = LogisticRegression(penalty='l2', tol=0.001, C=0.1, max_iter=8000, random_state=0)
     elif args.model_type == 'KNN':
         model = KNeighborsClassifier(n_neighbors=5)
+    elif args.model_type == 'GNB':
+        model = GaussianNB(var_smoothing=1e-1)
+    elif args.model_type == 'RF':
+        model = RandomForestClassifier(n_estimators=10)
     else:
         raise NotImplementedError
 
@@ -62,13 +71,17 @@ def main(args):
     print('Training completed.')
     print('Evaluating...')
 
+    preds = model.predict(X_train)
+    accuracy = accuracy_score(y_train, preds)
+    print('Accuracy on training data (same time period):',round(accuracy,3))
+
     preds = model.predict(X_val)
     accuracy = accuracy_score(y_val, preds)
-    print('Accuracy on validation data (same time period):',accuracy)
+    print('Accuracy on validation data (same time period):',round(accuracy,3))
 
     preds = model.predict(test_set.features)
     accuracy = accuracy_score(test_set.labels, preds)
-    print('Accuracy on test data (different time period):',accuracy)
+    print('Accuracy on test data (different time period):',round(accuracy,3))
 
     
 
